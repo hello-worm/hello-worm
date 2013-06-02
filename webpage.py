3a272390f196b8f1424b666e38951ca70b0e9fd6
brendajin <brendajin@gmail.com>
2013-06-01 19:29:58 -0700

    further conflicts merged

--- webpage.py
+++ webpage.py
@@ -4,7 +4,9 @@ import sqlite3
 
 app = Flask(__name__)
 
-
+def get_data():
+    conn = sqlite3.connect('worms.db')
+    c = conn.cursor()
     c.execute('''select * from worms order by id desc limit 1''')
     data = c.fetchone()
     [index, timestamp, temp, humidity, motion, imagestr] = data
@@ -19,7 +21,7 @@ app = Flask(__name__)
 def serve_home():
     
 
-    [index, timestamp, temp, humidity, motion, imagestr] = get_data();
+    [index, timestamp, temp, humidity, motion, imagestr] = get_data()
     
     return render_template('index.html', temp=str(temp), humidity=str(humidity))
 
